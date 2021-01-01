f1250d8a50b20e3affc235294d05fbf0f16177d9
rickparrish <rickparrish@users.noreply.github.com>
2021-01-01 18:40:42 -0500

    Delete books.js

--- js/books.js
+++
@@ -1,8 +0,0 @@
-$('#Books').DataTable({
-	"columnDefs": [
-		{ "type": "num", "targets": 0 },
-		{ "type": "num", "targets": 1 },
-	],
-	"lengthMenu": [[-1, 10, 25, 50], ["All", 10, 25, 50]],
-	"order": []
-});
